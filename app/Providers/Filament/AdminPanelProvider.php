3c8ca6aff5244697c2d1230048fcb9b432db4df5
Fathur Rizqi <fathurrizqizdjafar@gmail.com>
2026-05-30 17:05:39 +0800

    update tour apps

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -64,6 +64,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->renderHook(
+                'panels::body.end',
+                fn () => view('filament.onboarding-tour'),
+            );
     }
 }
